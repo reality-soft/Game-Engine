063d6386f6c103e1438be5e8299af4b661d0fbc3
DESKTOP-1GGFRU5\jiy12345 <jiy12367@gmail.com>
2023-01-19 15:37:38 +0900

    feat: premake 라이브러리 사용 가능하도록 추가
    - https://github.com/premake/premake-core 라이브러리 활용
    - 간단한 lua 코드 작성하여 구성 속성 설정 가능하도록 하기
    - bat 파일 만들어 현재 프로젝트에 대한 lua코드를 바탕으로 프로젝트 구성속성 설정하도록 하기

---
+++ premake5.lua
@@ -0,0 +1,102 @@
+workspace "Game-Engine"
+	architecture "x86_64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Engine"
+	location "Engine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"PLATFORM_WINDOWS",
+			"BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/TestGame")
+		}
+
+	filter "configurations:Debug"
+		defines "_DEBUG"
+		symbols "On"
+	filter "configurations:Release"
+		defines "_RELEASE"
+		optimize "On"
+	filter "configurations:Dist"
+		defines "_DIST"
+		optimize "On"
+
+
+project "TestGame"
+	location "TestGame"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Engine/vendor/spdlog/include",
+		"Engine/src"
+	}
+
+	links
+	{
+		"Engine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "_DEBUG"
+		symbols "On"
+	filter "configurations:Release"
+		defines "_RELEASE"
+		optimize "On"
+	filter "configurations:Dist"
+		defines "_DIST"
+		optimize "On"
