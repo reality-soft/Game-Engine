47ad59ef8692f51ccee3508a6d22394a9854ca1b
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2023-01-20 16:46:58 +0900

    팩토리 및 디바이스 정의, TK의 CommonStates 추가

--- Engine/src/Engine/DX11App.h
+++ Engine/src/Engine/DX11App.h
@@ -3,6 +3,7 @@
 #include <d3d11.h>
 #include <d3dcompiler.h>
 #include <dinput.h>
+#include "CommonStates.h"
 
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "d3dcompiler.lib")
@@ -12,5 +13,23 @@
 
 class DX11App
 {
+	SINGLETON(DX11App);
+
+private:
+    //=======================================================디바이스
+    ComPtr<IDXGIFactory>           dxgiFactory = nullptr;
+
+    ComPtr<ID3D11Device>           dx11Device = nullptr;
+    ComPtr<ID3D11DeviceContext>    dx11Context = nullptr;
+    ComPtr<IDXGISwapChain>         dx11SwapChain = nullptr;
+
+    ComPtr<ID3D11RenderTargetView> dx11RTView = nullptr;
+    ComPtr<ID3D11DepthStencilView> dx11DSView = nullptr;
+
+    DirectX::CommonStates DXStates;
+
+public:
+    D3D11_VIEWPORT                 viewPort;
+    float clearcolor[4] = { 0.03, 0.03, 0.03, 1 };
 
 };
