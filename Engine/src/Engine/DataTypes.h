11d0ed8471cc18111d7f1cd8fbc1c6c9914330cc
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2023-01-23 12:17:42 +0900

    feat: 두 가지 버텍스 타입 정의

---
+++ Engine/src/Engine/DataTypes.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "common.h"
+
+struct Vertex
+{
+	Vertex() = default;
+	~Vertex() = default;
+
+	XMFLOAT3   p; // 위치 12
+	XMFLOAT3   n; // 노말 24
+	XMFLOAT4   c; // 컬러 40
+	XMFLOAT2   t; // 텍스처 48
+
+
+};
+
+struct SkinnedVertex : public Vertex
+{
+	XMFLOAT4   i; // 인덱스 64
+	XMFLOAT4   w; // 가중치 80
+
+	SkinnedVertex operator +=(const Vertex& vertex)
+	{ 
+		p = vertex.p;
+		n = vertex.n;
+		c = vertex.c;
+		t = vertex.t;
+
+		return *this;
+	}
+};
