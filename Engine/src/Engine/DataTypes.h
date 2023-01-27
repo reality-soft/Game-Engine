17d623f5e514438fbae0648607f8f1adb840b99f
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2023-01-27 11:56:40 +0900

    feat : 새로운 데이터 타입 정의

--- Engine/src/Engine/DataTypes.h
+++ Engine/src/Engine/DataTypes.h
@@ -1,25 +1,21 @@
 #pragma once
 #include "common.h"
+#include <d3d11.h>
 
 namespace KGCA41B
 {
 	struct Vertex
 	{
-		Vertex() = default;
-		~Vertex() = default;
-
-		XMFLOAT3   p; // 위치 12
-		XMFLOAT3   n; // 노말 24
-		XMFLOAT4   c; // 컬러 40
-		XMFLOAT2   t; // 텍스처 48
-
-
+		XMFLOAT3   p;
+		XMFLOAT3   n;
+		XMFLOAT4   c;
+		XMFLOAT2   t;
 	};
 
 	struct SkinnedVertex : public Vertex
 	{
-		XMFLOAT4   i; // 인덱스 64
-		XMFLOAT4   w; // 가중치 80
+		XMFLOAT4   i;
+		XMFLOAT4   w;
 
 		SkinnedVertex operator +=(const Vertex& vertex)
 		{
@@ -30,5 +26,39 @@ namespace KGCA41B
 
 			return *this;
 		}
+	};
+
+	struct SingleMesh
+	{
+		vector<Vertex> vertices;
+		ID3D11Buffer* vertex_buffer;
+	};
+
+	struct CbTransform
+	{
+		XMMATRIX world_matrix;
+		XMMATRIX view_matrix;
+		XMMATRIX projection_matrix;
+	};
+
+	struct CbSkeleton
+	{
+		XMMATRIX  mat_skeleton[255];
+	};
+
+	enum class ActionType
+	{
+		JUMP,
+		SIT,
+		DASH,
+
+		ATTACK,
+		THROW,
+
+		SKILL1,
+		SKILL2,
+		SKILL3,
+
+		IDLE,
 	};
 }
