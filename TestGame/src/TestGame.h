bc87a3c6701e98c4f80c48f948aa1987a2ec7d37
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2023-01-31 13:14:30 +0900

    feat: 씬에서 fbx를 로드하고, ECS기반으로 오브젝트를 렌더링

--- TestGame/src/TestGame.h
+++ TestGame/src/TestGame.h
@@ -8,4 +8,22 @@ public:
 	virtual void OnUpdate();
 	virtual void OnRender();
 	virtual void OnRelease();
+
+public:
+	void CreatePlayer();
+	void CreateObjectFromFbx(string filepath);
+
+private:
+	entt::registry reg_scene;
+
+
+	entt::entity ent_player;
+	entt::entity ent_object;
+
+	KGCA41B::RenderSystem sys_render;
+	KGCA41B::CameraSystem sys_camera;
+	KGCA41B::InputSystem sys_input;
+	KGCA41B::AnimationSystem sys_animation;
+
 };
+
