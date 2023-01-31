19d4a64fc366aece33e67c126147466f40d19ef7
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2023-01-31 13:09:13 +0900

    fix: 렌더링 파이프라인 일부 수정

--- Engine/src/Engine/SingletonClass/DX11App.h
+++ Engine/src/Engine/SingletonClass/DX11App.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "common.h"
 #include <d3d11.h>
 #include <d3dcompiler.h>
@@ -52,3 +53,20 @@ private:
     D3D11_VIEWPORT                 view_port;
 
 };
+
+
+static XMMATRIX InterpolByTime(XMMATRIX& a, XMMATRIX& b, float t)
+{
+    XMMATRIX result;
+
+    for (int i = 0; i < 4; i++)
+    {
+        XMVECTOR row_a = XMLoadFloat4((XMFLOAT4*)&a.r[i]);
+        XMVECTOR row_b = XMLoadFloat4((XMFLOAT4*)&b.r[i]);
+        XMVECTOR row_result = XMVectorLerp(row_a, row_b, t);
+        XMStoreFloat4((XMFLOAT4*)&result.r[i], row_result);
+    }
+
+    return result;
+}
+
