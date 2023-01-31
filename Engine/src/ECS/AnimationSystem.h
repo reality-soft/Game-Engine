2a9bd6524799fd0beb4830a793077301b0126292
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2023-01-31 12:49:07 +0900

    feat: 애니메이션 시스템 추가

---
+++ Engine/src/ECS/AnimationSystem.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "System.h"
+
+namespace KGCA41B
+{
+	class DLL_API AnimationSystem : public System
+	{
+	public:
+		AnimationSystem();
+		~AnimationSystem();
+
+	public:
+		virtual void OnCreate(entt::registry& reg);
+		virtual void OnUpdate(entt::registry& reg);
+
+	private:
+		void PlayAnimation(Skeleton& skeleton, Animation& animation);
+
+		ID3D11DeviceContext* device_context = nullptr;
+	};
+}
