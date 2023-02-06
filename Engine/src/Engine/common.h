63cdd66e7b52cacb9a4d10947c388de617fe8c64
EussyKR <hang3137@naver.com>
2023-02-06 17:29:50 +0900

    feat : 문자열 split 함수 추가

--- Engine/src/Engine/common.h
+++ Engine/src/Engine/common.h
@@ -13,6 +13,7 @@
 #include <locale>
 #include <codecvt>
 #include <functional>
+#include <sstream>
 #include "fmod.hpp"
 
 using namespace std;
@@ -32,6 +33,7 @@ private:\
 type() {}\
 ~type() {}
 
+
 static std::wstring to_mw(const std::string& _src)
 {
 	USES_CONVERSION;
@@ -42,4 +44,16 @@ static std::string to_wm(const std::wstring& _src)
 {
 	USES_CONVERSION;
 	return std::string(W2A(_src.c_str()));
+}
+
+static std::vector<std::string> split(std::string input, char delimiter) {
+	std::vector<std::string> answer;
+	std::stringstream ss(input);
+	std::string temp;
+
+	while (getline(ss, temp, delimiter)) {
+		answer.push_back(temp);
+	}
+
+	return answer;
 }
