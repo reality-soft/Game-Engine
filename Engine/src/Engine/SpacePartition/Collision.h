890c4255d92e1ed8f49525ee19a3a398fdd40304
DESKTOP-1GGFRU5\jiy12345 <jiy12367@gmail.com>
2023-02-12 20:05:08 +0900

    feat: Collision 클래스 구현
    - 공간 분할에 쓰일 충돌 구현 공간 분할에서는 어떤 공간이 한 공간에 완전히 속하는지도 체크해야 하므로, 충돌의 종류를 분류해서 반환해 줄 수 있도록 함

---
+++ Engine/src/Engine/SpacePartition/Collision.h
@@ -0,0 +1,66 @@
+#pragma once
+#include "stdafx.h"
+#include "Shape.h"
+
+namespace KGCA41B {
+    enum class CollisionType
+    {
+        C_OUT = 0,
+        C_R_IN_L,
+        C_L_IN_R,
+        C_OVERLAP,
+    };
+
+    template<size_t Dimension>
+    class Collision
+    {
+    public:
+        static CollisionType CubeToCube(AABB<Dimension>& lhs, AABB<Dimension>& rhs)
+        {
+            float min_num;    float max_num;
+            Vector<Dimension> lhs_max_coord = lhs.MaxCoord();
+            Vector<Dimension> rhs_max_coord = rhs.MaxCoord();
+            for (int i = 0; i < Dimension; i++) {
+                min_num = lhs.MinCoord()[i] < rhs.MinCoord()[i] ? lhs.MinCoord()[i] : rhs.MinCoord()[i];
+                max_num = lhs_max_coord[i] > rhs_max_coord[i] ? lhs_max_coord[i] : rhs_max_coord[i];
+                if ((lhs.Size()[i] + rhs.Size()[i]) < (max_num - min_num))
+                    return CollisionType::C_OUT;
+            }
+
+            Vector<Dimension> min_coord, max_coord;
+            AABB<Dimension> intersect;
+
+            for (int i = 0; i < Dimension; i++) {
+                min_coord[i] = lhs.MinCoord()[i] > rhs.MinCoord()[i] ? lhs.MinCoord()[i] : rhs.MinCoord()[i];
+                max_coord[i] = lhs_max_coord[i] < rhs_max_coord[i] ? lhs_max_coord[i] : rhs_max_coord[i];
+            }
+
+            intersect.Set(min_coord, max_coord - min_coord);
+            if (intersect == lhs)
+                return CollisionType::C_L_IN_R;
+            if (intersect == rhs)
+                return CollisionType::C_R_IN_L;
+            return CollisionType::C_OVERLAP;
+        }
+        static CollisionType   SphereToSphere(nSphere<Dimension>& lhs, nSphere<Dimension>& rhs) {
+            float sum_radius = lhs.Radius() + rhs.Radius();
+            float sub_radius = fabs(lhs.Radius() - rhs.Radius());
+            Vector<Dimension> vDir = lhs.Center() - rhs.Center();
+            float distance = vDir.Length();
+
+            if (sum_radius < distance)
+            {
+                return CollisionType::C_OUT;
+            }
+            else if (sub_radius < distance && distance <= sum_radius)
+            {
+                return CollisionType::C_OVERLAP;
+            }
+            else {
+                if (lhs.Radius() > rhs.Radius()) return CollisionType::C_R_IN_L;
+                else                             return CollisionType::C_L_IN_R;
+            }
+        }
+    };
+
+}
