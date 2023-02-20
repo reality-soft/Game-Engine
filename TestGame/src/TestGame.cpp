a10fdf109dc40337dd538b56c2fafe9fd5bdacd5
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2023-02-20 15:42:18 +0900

    test : 테스트 코드 추가(공간분할 컬링)

--- TestGame/src/TestGame.cpp
+++ TestGame/src/TestGame.cpp
@@ -8,10 +8,20 @@ void TestGame::OnInit()
 	//	actor.OnInit(reg_scene, KGCA41B::AABB<3>(KGCA41B::Vector<3>::GetRandomVector(0, 300), KGCA41B::Vector<3>::GetRandomVector(5, 10)));
 	//	actor_list.push_back(actor);
 	//}
-	
 
 	DINPUT->Init(ENGINE->GetWindowHandle(), ENGINE->GetInstanceHandle());
-	KGCA41B::QUADTREE->Init(nullptr, nullptr, 4);
+	KGCA41B::RESOURCE->Init("../../Contents/");
+
+	XMINT2 level_size = { 5, 5 };
+	level.CreateLevel(pow(2, level_size.x) + 1, pow(2, level_size.y) + 1, 10, 100);
+	level.vs_id_ = "LevelVS.cso";
+	level.ps_id_ = "LevelPS.cso";
+	level.gs_id_ = "LevelGS.cso";
+	level.edit_mode = true;
+	level.texture_id = { "Ground.png" };
+
+
+	KGCA41B::QUADTREE->Init(&level, 4);
 	KGCA41B::FMOD_MGR->Init();
 	KGCA41B::RESOURCE->Init("../Contents");
 
@@ -24,21 +34,46 @@ void TestGame::OnInit()
 	ent_sound = reg_scene.create();
 
 
+	for (int i = 0; i < 100; ++i)
+	{
+		XMVECTOR random_loacation = { rand() % 100 - 50, rand() % 100 - 50, rand() % 100 - 50, 0 };
+		KGCA41B::AABBShape random_aabb = KGCA41B::AABBShape(random_loacation, 10.0f);
+		int node = KGCA41B::QUADTREE->UpdateNodeObjectBelongs(0, random_aabb, reg_scene.create());;
+		int a = 0;
+	}
+
 	//CreatePlayer();
 	//CreateCharacter();
 
 	//sys_render.OnCreate(reg_scene);
 	//sys_animation.OnCreate(reg_scene);
-	//sys_camera.TargetTag(reg_scene, "Player");
-	//sys_camera.OnCreate(reg_scene);
-	//sys_input.OnCreate(reg_scene);
+	KGCA41B::Camera debug_camera;
+	debug_camera.position = { 0, 100, -200, 0 };
+	debug_camera.look = { 0, -1, 0, 0 };
+	debug_camera.up = { 0, 1, 0, 0 };
+	debug_camera.near_z = 1.f;
+	debug_camera.far_z = 10000.f;
+	debug_camera.fov = XMConvertToRadians(45);
+	debug_camera.yaw = 0;
+	debug_camera.pitch = 0;
+	debug_camera.roll = 0;
+	debug_camera.speed = 100;
+	debug_camera.tag = "Player";
+	reg_scene.emplace<KGCA41B::Camera>(ent_player, debug_camera);
+
+	KGCA41B::InputMapping debug_input;
+	debug_input.tag = "Player";
+	reg_scene.emplace<KGCA41B::InputMapping>(ent_player, debug_input);
+
+	sys_camera.OnCreate(reg_scene);
+	sys_camera.TargetTag(reg_scene, "Player");
+	sys_input.OnCreate(reg_scene);
 
 }
 
 void TestGame::OnUpdate()
 {
 	KGCA41B::FMOD_MGR->Update();
-	int result = DINPUT->Update();
 
 	for (KGCA41B::Actor actor : actor_list) {
 		actor.OnUpdate(reg_scene);
@@ -46,10 +81,14 @@ void TestGame::OnUpdate()
 
 	sys_sound.OnUpdate(reg_scene);
 	sys_input.OnUpdate(reg_scene);
+	sys_camera.OnUpdate(reg_scene);
+
+	KGCA41B::QUADTREE->Frame(&sys_camera);
 }
 
 void TestGame::OnRender()
 {
+	KGCA41B::QUADTREE->Render();
 }
 
 void TestGame::OnRelease()
