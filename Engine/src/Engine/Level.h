a1da2c49802865e8bd52b35e6584cbe0a0213534
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2023-02-21 17:35:17 +0900

    feat : ID3D11Buffer를 가진 모든 구조체에 복사생성자 생성 (CopPtr::CopyTo())

--- Engine/src/Engine/Level.h
+++ Engine/src/Engine/Level.h
@@ -10,6 +10,12 @@ namespace KGCA41B
 {
 	struct CbHitCircle
 	{
+		CbHitCircle() = default;
+		CbHitCircle(const CbHitCircle& other)
+		{
+			data = other.data;
+			other.buffer.CopyTo(buffer.GetAddressOf());
+		}
 		struct Data
 		{
 			bool is_hit = false;
@@ -22,6 +28,12 @@ namespace KGCA41B
 
 	struct CbEditOption
 	{
+		CbEditOption() = default;
+		CbEditOption(const CbEditOption& other)
+		{
+			data = other.data;
+			other.buffer.CopyTo(buffer.GetAddressOf());
+		}
 		struct Data
 		{
 			XMINT4 altitude = {0, 0, 0, 0};
@@ -41,8 +53,35 @@ namespace KGCA41B
 	class DLL_API Level
 	{
 	public:
-		Level() {};
+		Level() = default;
+		Level(const Level& other)
+		{
+			texture_id.resize(other.texture_id.size());
+			texture_id = other.texture_id;
+
+			height_list_.resize(other.height_list_.size());
+			height_list_ = other.height_list_;
+
+			vs_id_ = other.vs_id_;
+			ps_id_ = other.ps_id_;
+			level_mesh_ = other.level_mesh_;
+			level_transform_ = other.level_transform_;
+			level_light_ = other.level_light_;
+			num_row_vertex_ = other.num_row_vertex_;
+			num_col_vertex_ = other.num_col_vertex_;
+			cell_distance_ = other.cell_distance_;
+			uv_scale_ = other.uv_scale_;
+
+			other.mip_map_sample.CopyTo(mip_map_sample.GetAddressOf());
+
+			height_field_shape_ = other.height_field_shape_;
+			height_field_collider_ = other.height_field_collider_;
+			height_field_body_ = other.height_field_body_;
+		}
 		~Level() {}
+
+	public:
+		bool ImportFromFile(string filepath);
 
 	public:
 		bool CreateLevel(UINT num_row, UINT num_col, int cell_distance, int uv_scale);
@@ -52,52 +91,38 @@ namespace KGCA41B
 		void Render(bool culling);
 
 		XMINT2 GetWorldSize();
-
-	public: // Editings
-		XMVECTOR LevelPicking(const MouseRay& mouse_ray, float circle_radius);
-		void LevelEdit(const MouseRay& mouse_ray, float circle_radius);
-		void Regenerate(UINT num_row, UINT num_col, int cell_distance, int uv_scale);
-		void ResetHeightField();
-		float sculpting_brush_ = 100.0f;
-
 		vector<Vertex> GetLevelVertex() { return level_mesh_.vertices; }
 		vector<UINT> GetLevelIndex() { return level_mesh_.indices; }
 
-	private:
+	protected:
 		void GenVertexNormal();
 		float GetHeightAt(float x, float y);
 		void GetHeightList();
 
 		XMFLOAT3 GetNormal(UINT i0, UINT i1, UINT i2);
 		bool CreateBuffers();
-		bool CreateEditBuffer(ID3D11Buffer** _buffer);
-
 
 	public:
-		bool edit_mode = false;
-
 		string vs_id_;
 		string ps_id_;
-		string gs_id_;
 		vector<string> texture_id;
 
-	private:
+	protected:
 		SingleMesh<Vertex> level_mesh_;
-		ComPtr<ID3D11Buffer> so_buffer_;
+		vector<float> height_list_;
+		ComPtr<ID3D11SamplerState> mip_map_sample;
 
 		CbTransform level_transform_;
 		CbLight level_light_;
-		CbHitCircle hit_circle_;
-		CbEditOption edit_option_;
 
 		UINT num_row_vertex_;
 		UINT num_col_vertex_;
 
 		int cell_distance_;
 		int uv_scale_;
-		vector<float> height_list_;
 
-	private:
+
+	protected:
 		reactphysics3d::HeightFieldShape* height_field_shape_ = nullptr;
 		reactphysics3d::Collider* height_field_collider_ = nullptr;
 		reactphysics3d::CollisionBody* height_field_body_ = nullptr;
