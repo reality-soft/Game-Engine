f09cda8a436c27b273327f874859734132cace1e
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2023-02-22 09:37:45 +0900

    fix : 쉐이더 수정. 레벨은 정점과 픽셀 만으로 렌더링하며 기하 쉐이더는 캡툴에서만 사용합니다.

--- TestGame/HLSL/LevelHeader.hlsli
+++ TestGame/HLSL/LevelHeader.hlsli
@@ -9,20 +9,7 @@ struct VS_IN
 struct VS_OUT
 {
 	float4 p : SV_POSITION;
-	float3 o : POSITION;
 	float3 n : NORMAL;
 	float4 c : COLOR;
 	float2 t : TEXCOORD0;
-
-	float1 strength : TEXCOORD1;
-	float4 circle : TEXCOORD2;
-};
-
-struct GS_IN
-{
-	float4 p : SV_POSITION;
-	float3 o : POSITION;
-	float3 n : NORMAL;
-	float4 c : COLOR;
-	float2 t : TEXCOORD;
 };
--- TestGame/HLSL/LevelVS.hlsl
+++ TestGame/HLSL/LevelVS.hlsl
@@ -11,32 +11,9 @@ cbuffer cb_viewproj : register(b1)
 	matrix g_matProj;
 }
 
-cbuffer cb_hitcircle : register(b2)
-{
-	bool   is_hit;
-	float  circle_radius;
-	float4 hitpoint;
-	float4 circle_color;
-}
-
 VS_OUT VS(VS_IN input)
 {
 	VS_OUT output = (VS_OUT)0;
-	
-	if (is_hit)
-	{
-		float length_from_point = length((input.p - hitpoint));
-		if (length_from_point <= circle_radius)
-		{
-			output.strength =  1.0f - length_from_point / circle_radius;
-			output.circle = float4((output.strength * 3) + 1, 1.0f, 1.0f, 1.0f);
-		}
-		else
-		{
-			output.strength = 0.0f;
-			output.circle = float4(1.0f, 1.0f, 1.0f, 1.0f);
-		}
-	}
 
 	float4 vLocal = float4(input.p, 1.0f);
 	float4 vWorld = mul(vLocal, g_matWorld);
@@ -44,9 +21,7 @@ VS_OUT VS(VS_IN input)
 	float4 vProj = mul(vView, g_matProj);
 
 	output.p = vProj;
-	output.o = input.p;
 	output.n = input.n;
-	output.c = input.c * output.circle;
 	output.t = input.t;
 
 	return output;
