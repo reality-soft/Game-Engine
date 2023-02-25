886521ac7bc8211d114958ab995fe2ecb5d0b9ed
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2023-02-25 21:04:50 +0900

    feat : Surface 클래스 제거

--- TestGame/src/TestGame.cpp
+++ TestGame/src/TestGame.cpp
@@ -64,9 +64,50 @@ void TestGame::OnUpdate()
 {
 	KGCA41B::FMOD_MGR->Update();
 
-	for (KGCA41B::StaticObject actor : actor_list) {
-		actor.OnUpdate(reg_scene);
-	}
+	FbxMgr::GetInst()->ImportAndSaveFbx("../../Contents/STM/LeeEnfieldMKIII.fbx");
+
+	test_object.transform = XMMatrixIdentity();
+	C_Transform transform;
+	transform.local = XMMatrixIdentity();
+	transform.world = XMMatrixIdentity();
+
+	test_object.OnInit(reg_scene, transform, AABBShape(XMVectorZero(), 10.0f), "LeeEnfieldMKIII.stmesh");
+
+	KGCA41B::Material mat1;
+
+	KGCA41B::Material mat1, mat2, mat3, mat4, mat5, mat6, mat7;
+	mat1.texture_id = {   
+	"Bayonet_DF.png",
+	};
+	mat2.texture_id = {
+	"Ammo_DF.png",
+	};
+	mat3.texture_id = {
+	"Bolt_DF.png",
+	};
+	mat4.texture_id = {
+	"Magazine_DF.png",
+	};
+	mat5.texture_id = {
+	"Metal_DF.png",
+	};
+	mat6.texture_id = {
+	"Wood_DF.png",
+	};
+	mat7.texture_id = {
+	"Strap_DF.png",
+	};
+
+	surface_definer.test_surface.materials.push_back(mat1);
+	surface_definer.test_surface.materials.push_back(mat2);
+	surface_definer.test_surface.materials.push_back(mat3);
+	surface_definer.test_surface.materials.push_back(mat4);
+	surface_definer.test_surface.materials.push_back(mat5);
+	surface_definer.test_surface.materials.push_back(mat6);
+	surface_definer.test_surface.materials.push_back(mat7);
+
+	surface_definer.test_surface.CreateSurface();
+}
 
 	sys_sound.OnUpdate(reg_scene);
 	sys_camera.OnUpdate(reg_scene);
