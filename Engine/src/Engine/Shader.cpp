6381f39abde615351e5ea8461db3dd681e22d357
EussyKR <hang3137@naver.com>
2023-02-28 10:08:46 +0900

    feat : rand 매크로 추가

--- Engine/src/Engine/Shader.cpp
+++ Engine/src/Engine/Shader.cpp
@@ -111,8 +111,8 @@ bool GeometryShader::LoadCompiled(wstring _csoFIle)
     D3D11_SO_DECLARATION_ENTRY ied[] =
     {
         { 0, "SV_POSITION", 0, 0, 4, 0 },
-        { 0, "POSITION",    0, 0, 3, 0 },
-        { 0, "NORMAL",      0, 0, 3, 0 },
+        //{ 0, "POSITION",    0, 0, 3, 0 },
+        //{ 0, "NORMAL",      0, 0, 3, 0 },
         { 0, "COLOR",       0, 0, 4, 0 },
         { 0, "TEXCOORD",    0, 0, 2, 0 }
     };
