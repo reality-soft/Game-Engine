9bddf8d95a6602718b34f8b084f0d476724c7a60
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2023-03-14 14:05:15 +0900

    feat: 플레이어 캐릭터의 씬에 추가 및 키 바인딩

--- TestGame/src/TestGame.h
+++ TestGame/src/TestGame.h
@@ -16,8 +16,6 @@ private:
 	SkySphere sky_sphere;
 	LightMeshLevel level;
 
-	entt::registry reg_scene;
-
 	reality::LightingSystem sys_light;
 	reality::RenderSystem sys_render;
 	reality::CameraSystem sys_camera;
