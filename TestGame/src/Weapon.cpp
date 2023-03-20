59178c93752be81d3c9bffc1eab1ee1d4d57fd06
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2023-03-20 20:08:39 +0900

    chore: 무기 위치 미세 조정

--- TestGame/src/Weapon.cpp
+++ TestGame/src/Weapon.cpp
@@ -14,6 +14,8 @@ void Weapon::OnInit(entt::registry& registry)
 	registry.emplace_or_replace<reality::C_StaticMesh>(entity_id_, stm);
 
 	transform_tree_.root_node = make_shared<TransformTreeNode>(TYPE_ID(C_StaticMesh));
+	
+	socket_offset_ = XMMatrixRotationY(XMConvertToRadians(90)) * XMMatrixTranslationFromVector({-20, 3, -5, 0});
 
 	transform_matrix_ = XMMatrixTranslation(0, 100, 0);
 	transform_tree_.root_node->OnUpdate(registry, entity_id_, transform_matrix_);
@@ -29,6 +31,7 @@ void Weapon::OnUpdate()
 	transform_matrix_ = owner_character->GetTransformMatrix();
 
 	C_Animation* animation_component = reg_scene_->try_get<C_Animation>(owner_character->GetEntityId());
+	C_Camera* camera_component = reg_scene_->try_get<C_Camera>(owner_character->GetEntityId());
 
 	if (animation_component != nullptr && socket_id_ != -1) {
 		OutAnimData* anim_data = RESOURCE->UseResource<OutAnimData>(animation_component->anim_id);
@@ -38,7 +41,9 @@ void Weapon::OnUpdate()
 		}
 	}
 
-	transform_tree_.root_node->OnUpdate(*reg_scene_, entity_id_, socket_offset_ * animation_matrix_ * owner_transform_ * transform_matrix_);
+	XMMATRIX rotation_matrix = XMMatrixRotationY(camera_component->pitch_yaw.y);
+
+	transform_tree_.root_node->OnUpdate(*reg_scene_, entity_id_, socket_offset_ * animation_matrix_ * rotation_matrix * owner_transform_ * transform_matrix_);
 }
 
 void Weapon::SetOwnerId(entt::entity owner_id)
