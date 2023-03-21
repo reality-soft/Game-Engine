564218cffa681af62cee0718976d7502b6ec8b74
EussyKR <hang3137@naver.com>
2023-03-21 13:17:20 +0900

    feat : 테스트 게임에 FX_BloodImpact 클래스 추가

---
+++ TestGame/src/FX_BloodImpact.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "FX_BloodImpact.h"
+#include "SceneMgr.h"
+#include "ResourceMgr.h"
+
+using namespace reality;
+
+void FX_BloodImpact::OnInit(entt::registry& registry)
+{
+	FX_BaseEffectActor::OnInit(registry);
+
+	auto& effect_comp = registry.get<C_Effect>(GetEntityId());
+	effect_comp.effect_id = "sample_effect";
+	effect_comp.effect = *RESOURCE->UseResource<Effect>(effect_comp.effect_id);
+}
+
+void FX_BloodImpact::OnUpdate()
+{
+	FX_BaseEffectActor::OnUpdate();
+}
