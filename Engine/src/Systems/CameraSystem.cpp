881827a21dee03cc844bac7b24e31b4e01720080
EussyKR <hang3137@naver.com>
2023-03-23 16:00:53 +0900

    chore : 카메라 위치 설정

--- Engine/src/Systems/CameraSystem.cpp
+++ Engine/src/Systems/CameraSystem.cpp
@@ -241,7 +241,7 @@ void CameraSystem::CreateMatrix()
 	}
 	else {
 		rotation_center = camera->target_pos;
-		view_matrix = XMMatrixLookAtLH(camera->camera_pos, camera->target_pos, up_vector);
+		view_matrix = XMMatrixLookAtLH(camera->camera_pos + XMVECTOR{ 10, 0, 10, 0 }, camera->target_pos + XMVECTOR{ 10, 0, 10, 0 }, up_vector);
 		rotation_matrix = DirectX::XMMatrixAffineTransformation(scale_vector, rotation_center, rotation_quaternion, XMVectorZero());
 		rotation_matrix = XMMatrixInverse(0, rotation_matrix);
 		view_matrix = XMMatrixMultiply(rotation_matrix, view_matrix);
