c9946afc7de34f52b148bf33b66049101edb4598
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2023-03-28 11:41:49 +0900

    fix : 쉐이더에서 정점컬러 값 삭제

--- TestGame/src/TestGame.cpp
+++ TestGame/src/TestGame.cpp
@@ -16,10 +16,12 @@ void TestGame::OnInit()
 	reality::RESOURCE->Init("../../Contents/");
 
 	//FbxImportOption option;
-	//option.import_rotation = {90, 180, 0, 0};
-	//option.import_scale = -10.0f;
-	//reality::FBX->ImportAndSaveFbx("../../Contents/FBX/DeadPoly_FullLevel_03.fbx", option);
-	//reality::FBX->ImportAndSaveFbx("../../Contents/FBX/DeadPoly_Level_Collision_03.fbx", option);
+	//option.import_rotation = {0, 0, 0, 0};
+	//option.import_scale = 10.0f;
+	//option.recalculate_normal = true;
+
+	//reality::FBX->ImportAndSaveFbx("../../Contents/FBX/DeadPoly_FullLevel_03.fbx", option, FbxVertexOption::BY_POLYGON_VERTEX);
+	//reality::FBX->ImportAndSaveFbx("../../Contents/FBX/DeadPoly_Level_Collision_03.fbx", option, FbxVertexOption::BY_POLYGON_VERTEX);
 
 	WRITER->Init();
 	reality::ComponentSystem::GetInst()->OnInit(reg_scene_);
@@ -62,7 +64,7 @@ void TestGame::OnInit()
 	INPUT_EVENT->SubscribeMouseEvent({ MouseButton::L_BUTTON }, idle, KEY_UP);
 
 	sky_sphere.CreateSphere();
-	level.Create("DeadPoly_FullLevel_03.ltmesh", "LevelVS.cso", "LevelGS.cso", "DeadPoly_Level_Collision_03.ltmesh");
+	level.Create("DeadPoly_FullLevel_03.stmesh", "LevelVS.cso", "DeadPoly_Level_Collision_03.stmesh");
 	//level.ImportGuideLines("../../Contents/BinaryPackage/DeadPoly_Blocking1.mapdat", GuideLine::GuideType::eBlocking);
 	level.ImportGuideLines("../../Contents/BinaryPackage/DeadPoly_NpcTrack.mapdat", GuideLine::GuideType::eNpcTrack);
 
