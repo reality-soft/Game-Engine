b8e5ccd05ac31e59e134b7388dc2ce20bc028a7d
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2023-03-28 11:41:28 +0900

    fix : 레벨 쉐이더에서 기하 쉐이더 삭제

--- TestGame/HLSL/LevelVS.hlsl
+++ TestGame/HLSL/LevelVS.hlsl
@@ -3,17 +3,19 @@
 struct VS_IN
 {
     float3 p : F3_POSITION;
+    float3 n : F3_NORMAL;
     float2 t : F2_TEXTURE;
 };
 
 struct VS_OUT
 {
-    float4 p : SV_POSITION;  
-    float2 t : TEXCOORD;
-    float lod : TEXCOORD1;
+    float4 p : SV_POSITION;
+    float3 n : NORMAL0;
+    float2 t : TEXCOORD0;
     
+    float lod : TEXCOORD1;    
     float3 view_dir : TEXCOORD2;
-    matrix view_proj : TEXCOORD3;
+    float3 origin : NORMAL1;
 };
 
 VS_OUT VS(VS_IN input)
@@ -22,13 +24,15 @@ VS_OUT VS(VS_IN input)
     
     float4 local = float4(input.p, 1.0f);    
     float4 world = mul(local, IdentityMatrix());
+    float4 project = mul(world, ViewProjection());
 
-    output.p = world;
+    output.p = project;
+    output.n = input.n;
     output.t = input.t;
-    output.lod = GetLod(input.p);
     
-    output.view_proj = ViewProjection();
+    output.lod = GetLod(input.p);    
     output.view_dir = normalize(camera_world - world).xyz;
+    output.origin = world;
 
     return output;
 }
