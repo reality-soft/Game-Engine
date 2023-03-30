aefb0a5692edfa355ad110f19edc47f34dd5dcb4
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2023-03-30 12:54:33 +0900

    feat : 쉐이더 수정 및 테스트

--- TestGame/HLSL/LevelPS.hlsl
+++ TestGame/HLSL/LevelPS.hlsl
@@ -23,7 +23,7 @@ float4 PS(PS_OUT input) : SV_Target
     albedo = ChangeValue(albedo, 0.5f);
     albedo = ApplyHemisphericAmbient(input.n, albedo);
     
-    final_color = ApplyCookTorrance(albedo, 0.6f, 0.2f, input.n, input.view_dir);
+    final_color = ApplyCookTorrance(albedo, 0.6f, input.n, input.view_dir);
     final_color = ApplyDistanceFog(final_color, input.origin);
     return final_color;
 }
