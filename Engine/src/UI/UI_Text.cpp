35754fd9a4965490cf18f54246a48dccebecc32d
EussyKR <hang3137@naver.com>
2023-04-03 21:07:55 +0900

    feat : WriteMgr에서 폰트를 선택해서 텍스트 출력하도록 기능 추가

--- Engine/src/UI/UI_Text.cpp
+++ Engine/src/UI/UI_Text.cpp
@@ -1,19 +1,29 @@
 #include "stdafx.h"
 #include "UI_Text.h"
-#include "WriteMgr.h"
 
 using namespace reality;
 
-void UI_Text::InitText(string text, XMFLOAT2 pos, float size, XMFLOAT4 color)
+void UI_Text::InitText(string text, E_Font font, XMFLOAT2 pos, float size, XMFLOAT4 color)
 {
 	UIBase::Init();
 	text_ = text;
+	font_ = font;
 	SetLocalRectByMin(pos, 100.0f, 100.0f);
 	size_ = size;
 	color_ = color;
 }
 
+void UI_Text::SetText(string text)
+{
+	text_ = text;
+}
+
+void UI_Text::SetFont(E_Font font)
+{
+	font_ = font;
+}
+
 void UI_Text::RenderThisUI()
 {
-	WRITER->Draw(text_, rect_transform_.world_rect.min, size_, color_);
+	WRITER->Draw(text_, font_, rect_transform_.world_rect.min, size_, color_);
 }
--- Engine/src/UI/WriteMgr.h
+++ Engine/src/UI/WriteMgr.h
@@ -5,8 +5,16 @@
 
 #include <SpriteFont.h>
 
+
+
 namespace reality
 {
+	enum E_Font
+	{
+		BASIC = 0,
+		ROTUNDA = 1,
+	};
+
 	class DLL_API WriteMgr
 	{
 		SINGLETON(WriteMgr);
@@ -14,11 +22,12 @@ namespace reality
 
 	private:
 		unique_ptr<SpriteBatch> sprite_batch_;
-		unique_ptr<SpriteFont>	sprite_font_;
+		unique_ptr<SpriteFont>	sprite_font_basic_;
+		unique_ptr<SpriteFont>	sprite_font_rotunda_;
 	public:
 		void Init();
 
-		void Draw(string text, XMFLOAT2 coord, float size = 1.0f, XMFLOAT4 color = {1.0f, 1.0f, 1.0f, 1.0f});
+		void Draw(string text, E_Font font, XMFLOAT2 coord = { 0.0f, 0.0f }, float size = 1.0f, XMFLOAT4 color = { 1.0f, 1.0f, 1.0f, 1.0f });
 	};
 }
 
