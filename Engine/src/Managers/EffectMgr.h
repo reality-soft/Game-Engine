9fcf4f2ac3eab71925a388931067ecdd8655a775
EussyKR <hang3137@naver.com>
2023-04-06 13:41:49 +0900

    feat : 씬 타입을 통해 해당하는 씬에 이펙트를 생성하는 메소드 추가

--- Engine/src/Managers/EffectMgr.h
+++ Engine/src/Managers/EffectMgr.h
@@ -15,6 +15,13 @@ namespace reality
 			auto entity = SCENE_MGR->AddActor<EffectActorClass>();
 			return SCENE_MGR->GetActor<EffectActorClass>(entity);
 		}
+		template<typename EffectActorClass>
+		EffectActorClass* MakeEffectActor(E_SceneType type)
+		{
+			auto entity = SCENE_MGR->GetScene(type)->AddActor<EffectActorClass>();
+			return SCENE_MGR->GetScene(type)->GetActor<EffectActorClass>(entity);
+		}
+	
 	public:
 		// Effect
 		template<typename EffectActorClass>
@@ -35,6 +42,23 @@ namespace reality
 
 			effect_actor->Spawn(pos, rot_q, scale);
 		}
-		
+		template<typename EffectActorClass>
+		void SpawnEffect(E_SceneType type, XMVECTOR pos, XMVECTOR rot_q = XMQuaternionIdentity(), XMVECTOR scale = XMVectorSet(1.0f, 1.0f, 1.0f, 1.0f))
+		{
+			EffectActorClass* effect_actor = MakeEffectActor<EffectActorClass>(type);
+			effect_actor->Spawn(pos, rot_q, scale);
+		}
+		template<typename EffectActorClass>
+		void SpawnEffectFromNormal(E_SceneType type, XMVECTOR pos, XMVECTOR normal, XMVECTOR scale = XMVectorSet(1.0f, 1.0f, 1.0f, 1.0f))
+		{
+			EffectActorClass* effect_actor = MakeEffectActor<EffectActorClass>(type);
+
+			XMVECTOR base = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+			XMVECTOR axis = XMVector3Cross(base, normal);
+			float angle = acosf(XMVectorGetX(XMVector3Dot(base, normal)));
+			XMVECTOR rot_q = XMQuaternionRotationAxis(axis, angle);
+
+			effect_actor->Spawn(pos, rot_q, scale);
+		}
 	};
 }
