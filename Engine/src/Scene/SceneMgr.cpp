5c4c3547ba00a812264b82e1897b70545386bf34
EussyKR <hang3137@naver.com>
2023-04-06 13:41:30 +0900

    feat : 신 매니저에 있던 Actors와 Players를 분리하고 enum E_SceneType을 통해 씬 정보를 받아올 수 있도록 변경

--- Engine/src/Scene/SceneMgr.cpp
+++ Engine/src/Scene/SceneMgr.cpp
@@ -10,30 +10,51 @@ namespace reality{
 	{
 		return weak_ptr<Scene>(cur_scene_);
 	}
+	shared_ptr<Scene> SceneMgr::GetScene(E_SceneType type)
+	{
+		if (scenes.find(type) == scenes.end())
+			return NULL;
+		else
+			return scenes[type];
+	}
+	void SceneMgr::ChangeScene(E_SceneType scene_type)
+	{
+		scene_to_change_ = scene_type;
+	}
 	bool SceneMgr::DestroyActor(entt::entity actor_id)
 	{
-		if (actors_.find(actor_id) == actors_.end())
+		if (cur_scene_->GetActors().find(actor_id) == cur_scene_->GetActors().end())
 			return false;
 		GetRegistry().destroy(actor_id);
-		actors_.erase(actor_id);
+		cur_scene_->GetActors().erase(actor_id);
 		return true;
+	}
+	void SceneMgr::InternalChangeScene()
+	{
+		if (scenes.find(scene_to_change_) != scenes.end())
+		{
+			cur_scene_ = scenes[scene_to_change_];
+			COMPONENT->OnInit(cur_scene_->GetRegistryRef());
+		}
+		scene_to_change_ = E_SceneType::SCENE_NONE;
 	}
 	void SceneMgr::OnInit()
 	{
 		if (cur_scene_.get() != nullptr) {
 			cur_scene_->OnInit();
+			COMPONENT->OnInit(cur_scene_->GetRegistryRef());
 		}
-		COMPONENT->OnInit(cur_scene_->GetRegistryRef());
 	}
 
 	void SceneMgr::OnUpdate()
 	{
 		if (cur_scene_.get() != nullptr) {
-			cur_scene_->OnUpdate();
+			cur_scene_->OnBaseUpdate();
 		}
 
-		for (const auto& actor : actors_) {
-			actor.second->OnUpdate();
+		if (scene_to_change_ != E_SceneType::SCENE_NONE)
+		{
+			InternalChangeScene();
 		}
 	}
 
@@ -52,6 +73,6 @@ namespace reality{
 	}
 
 	int  SceneMgr::GetNumOfActor() {
-		return actors_.size();
+		return cur_scene_->GetActors().size();
 	}
 }
