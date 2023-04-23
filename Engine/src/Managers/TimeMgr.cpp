8bd572194655f63a7a4981857fbf07b6fc38ccec
JasontreksC <106058189+JasontreksC@users.noreply.github.com>
2023-04-24 01:09:25 +0900

    chore : fps를 카운트하는 방식 변경

--- Engine/src/Managers/TimeMgr.cpp
+++ Engine/src/Managers/TimeMgr.cpp
@@ -17,11 +17,23 @@ void TimeMgr::Update()
 	QueryPerformanceCounter((LARGE_INTEGER*)&cur_time);
 	delta_time = ((double)cur_time.QuadPart - (double)last_time.QuadPart) / (double)period_frequency.QuadPart;
 
-	fps = 0;
-	for (double i = 0; i < 1.0; i += delta_time)
+	static int fps_ = 0;
+	if (deltas < 1.0f)
 	{
-		fps++;
+		deltas += delta_time;
+		fps_++;
 	}
+	else
+	{
+		deltas = 0.0f;
+		fps = fps_;
+		fps_ = 0;
+	}
+	//fps = 0;
+	//for (double i = 0; i < 1.0; i += delta_time)
+	//{
+	//	fps++;
+	//}
 
 	game_time += delta_time;
 	last_time = cur_time;
@@ -32,7 +44,7 @@ double TimeMgr::GetDeltaTime()
 	return delta_time;
 }
 
-float TimeMgr::GetFPS()
+int TimeMgr::GetFPS()
 {
 	return fps;
 }
